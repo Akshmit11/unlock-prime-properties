983f9da6a4da57d779ea6d6cb78eb49d45c67a76
Akshmit <saxena.akshmit@gmail.com>
2024-05-30 17:15:56 +0530

    clerk webhook added

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Real Estate Listing Site",
+  title: "Unlock Prime Properties",
   description: "List your property",
 };
 
